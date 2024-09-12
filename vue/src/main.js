ce9aafd7c16261ca6bdc15d4dcd2d8730a85613e
zzb-666-q <1278369564@qq.com>
2024-09-12 20:12:46 +0800

    更新

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,20 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 // require('dotenv').config();
 
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import { Notification, MessageBox, Message, Loading } from 'element-ui'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueCookies from 'vue-cookies';
 
-import axios from "axios";
-import VueAxios from "vue-axios";
-import VueCookies from "vue-cookies";
-
-import { tokenUrl } from "./tokenUrl/tokenUrl.js";
-import { parseTime, resetForm, addDateRange, selectDictLabel, selectDictLabels, handleTree, completionImg } from "@/utils/slms";
+import {
+  parseTime,
+  resetForm,
+  addDateRange,
+  selectDictLabel,
+  selectDictLabels,
+  handleTree,
+  completionImg,
+} from '@/utils/slms';
 // const process =require("dotenv").config({ path: '.env' });
 
 // 全局方法挂载
@@ -31,10 +36,10 @@ Vue.use(VueAxios, axios);
 Vue.use(VueCookies);
 
 // 分页组件
-import Pagination from "@/components/Pagination";
+import Pagination from '@/components/Pagination';
 // 图片上传组件
-import ImageUploader from "@/components/ImageUploader";
-import imgUpload from "@/components/imgUpload";
+import ImageUploader from '@/components/ImageUploader';
+import imgUpload from '@/components/imgUpload';
 
 // 全局组件挂载
 Vue.component('Pagination', Pagination);
@@ -43,65 +48,10 @@ Vue.component('imgUpload', imgUpload);
 
 axios.defaults.baseURL = '/apis';
 
-// // 添加请求拦截器
-axios.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
-    // console.log('config ==> ', config);
-
-    if (tokenUrl.indexOf(config.url) > -1) {
-      //获取cookie
-      let token = Vue.$cookies.get("token");
-      //将token挂在到请求头中
-      config.headers.token = token;
-    }
-    return config;
-  },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  }
-);
-// 添加响应拦截器
-axios.interceptors.response.use(
-  function (response) {
-    let data = {
-      data: response.data,
-      status: response.status,
-    };
-    // 未设置状态码则默认成功状态
-    const code = response.data.code || 200;
-    // 获取错误信息
-    const msg = response.data.msg;
-    if (code === 200) {
-      Message({
-        message: msg,
-        type: 'success'
-      })
-    } else if (code === 500) {
-      Message({
-        message: msg,
-        type: 'error'
-      })
-    } else if (code === 300) {
-      Message({
-        message: msg,
-        type: 'warning'
-      })
-    }
-    // 对响应数据做点什么
-    return response;
-  },
-  function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-  }
-);
-
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,25 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // webpack-dev-server 相关配置
-
   devServer: {
     host: 'localhost',
-    port: 8080,//设置默认启动端口号
-    open: true,//自动打开浏览器
+    port: 8080, //设置默认启动端口号
+    open: true, //自动打开浏览器
     proxy: {
-      '/apis': {   
+      '/apis': {
         // target: 'http://127.0.0.1:9000',  // 接口域名
-        target: process.env.VUE_APP_BASE_API,  // 接口域名
-        secure: false,  // 如果是https接口，需要配置这个参数
+        target: process.env.VUE_APP_BASE_API, // 接口域名
+        secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true,
-        ws: true,  //是否跨域
+        ws: true, //是否跨域
         pathRewrite: {
-          '^/apis': ''   //需要rewrite的,
-        }
-      }
+          '^/apis': '', //需要rewrite的,
+        },
+      },
     },
-  }
-})
-
+  },
+});
